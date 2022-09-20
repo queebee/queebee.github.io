ba280e08b0943cc0fbaa6daab354a95b5bf5557a
queebee <20BCS4107@cuchd.in>
2022-09-21 00:49:59 +0530

    update

---
+++ siddhant-portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Siddhant Saraswat</span> Currently, I'm enrolled at Chandigarh University studying Cloud Computing (IBM). Creative and passionate about art, I became intrigued with digital art and then 3D-modeling and game design/development.<br><br> As a blender and Unreal developer, I have had a lot of experience. I already have experience in this side of the platform, but I wanted to expand this to the cloud to allow all users to access high-end software and games on low-end devices. To do this, I have experience on Amazon Web Services, Google Cloud Platform, and IBM cloud environments."
+      ],
+      typeSpeed: 8.5,
+      contentType: 'html'
+    });
+  }, 100);
